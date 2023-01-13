0aae4c1261994cfb702ce5db45f920d619ace0bd
suyog-pipliwaal <suyogpipliwal@gmail.com>
2023-01-13 15:04:56 +0000

    bug fixed

--- config/database.js
+++ config/database.js
@@ -11,14 +11,14 @@ const pool = mariadb.createPool({
     database:process.env.DB_NAME,
     connectionLimit: 5
 });
-// const pool = mariadb.createPool({
-//     host: 'localhost',
-//     port: '3306',
-//     user: 'qmul',
-//     password:'F0r906yr@',
-//     database:'dialogplus',
-//     connectionLimit: 5
-// });
+/*const pool = mariadb.createPool({
+    host: 'localhost',
+    port: '3306',
+    user: 'qmul',
+    password:'F0r906yr@',
+    database:'dialogplus',
+    connectionLimit: 5
+});*/
 module.exports={
     getConnection: function(){
         return new Promise(function(resolve,reject){
