719cc32ccc0344c4e461ae2379fe190142cddeb8
suyog-pipliwaal <suyogpipliwal@gmail.com>
2023-09-20 10:59:46 +0100

    new build updated

---
+++ dialogplus/build/precache-manifest.9d214a766024503e9cce03ebc713459c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "afac4ea0aa09a5d33f7b",
+    "url": "/static/css/main.23d099d4.chunk.css"
+  },
+  {
+    "revision": "afac4ea0aa09a5d33f7b",
+    "url": "/static/js/main.afac4ea0.chunk.js"
+  },
+  {
+    "revision": "d79c12704244fbcdc686",
+    "url": "/static/js/1.d79c1270.chunk.js"
+  },
+  {
+    "revision": "ed02e56bf96d61e429cf",
+    "url": "/static/css/2.a50236c2.chunk.css"
+  },
+  {
+    "revision": "ed02e56bf96d61e429cf",
+    "url": "/static/js/2.ed02e56b.chunk.js"
+  },
+  {
+    "revision": "54c867368d9513164bf0",
+    "url": "/static/js/3.54c86736.chunk.js"
+  },
+  {
+    "revision": "dd173c22cdd6d78313df",
+    "url": "/static/js/4.dd173c22.chunk.js"
+  },
+  {
+    "revision": "4ead5a12603f4ed92e3c",
+    "url": "/static/js/5.4ead5a12.chunk.js"
+  },
+  {
+    "revision": "7ed10484197bb6f74657",
+    "url": "/static/js/runtime~main.7ed10484.js"
+  },
+  {
+    "revision": "4d7991d912e554edb68e959c318fdcae",
+    "url": "/index.html"
+  }
+];
