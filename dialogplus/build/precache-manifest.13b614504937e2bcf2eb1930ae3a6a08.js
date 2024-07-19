c436f632e52833088ec0795203cae1116360303d
suyog-pipliwaal <suyogpipliwal@gmail.com>
2024-07-19 14:03:03 +0100

    new version update

---
+++ dialogplus/build/precache-manifest.13b614504937e2bcf2eb1930ae3a6a08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae62c681a43ff5733fd1",
+    "url": "/static/css/main.ec9fb099.chunk.css"
+  },
+  {
+    "revision": "ae62c681a43ff5733fd1",
+    "url": "/static/js/main.ae62c681.chunk.js"
+  },
+  {
+    "revision": "598021718dfa00d5b500",
+    "url": "/static/js/1.59802171.chunk.js"
+  },
+  {
+    "revision": "fcb89506108dc40d4bbe",
+    "url": "/static/css/2.c1787268.chunk.css"
+  },
+  {
+    "revision": "fcb89506108dc40d4bbe",
+    "url": "/static/js/2.fcb89506.chunk.js"
+  },
+  {
+    "revision": "86b60f0748d7cc6ed688",
+    "url": "/static/js/runtime~main.86b60f07.js"
+  },
+  {
+    "revision": "0e332b1d15e4b93ff054ea9377e56599",
+    "url": "/index.html"
+  }
+];
--- dialogplus/build/service-worker.js
+++ dialogplus/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.305a3cd503d729c72eaf2286aad453ee.js"
+  "/precache-manifest.13b614504937e2bcf2eb1930ae3a6a08.js"
 );
 
 workbox.clientsClaim();
