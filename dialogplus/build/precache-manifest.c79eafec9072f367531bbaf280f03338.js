62956335640abc5a7c020183b08aec01c1402d5f
suyog-pipliwaal <suyogpipliwal@gmail.com>
2024-07-29 11:38:02 +0100

    build process done

---
+++ dialogplus/build/precache-manifest.c79eafec9072f367531bbaf280f03338.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f25ef38406187ded05b",
+    "url": "/static/css/main.76452b16.chunk.css"
+  },
+  {
+    "revision": "9f25ef38406187ded05b",
+    "url": "/static/js/main.9f25ef38.chunk.js"
+  },
+  {
+    "revision": "598021718dfa00d5b500",
+    "url": "/static/js/1.59802171.chunk.js"
+  },
+  {
+    "revision": "b161a568e574e952a023",
+    "url": "/static/css/2.baefb67c.chunk.css"
+  },
+  {
+    "revision": "b161a568e574e952a023",
+    "url": "/static/js/2.b161a568.chunk.js"
+  },
+  {
+    "revision": "86b60f0748d7cc6ed688",
+    "url": "/static/js/runtime~main.86b60f07.js"
+  },
+  {
+    "revision": "c379e74e98a619e50c0b47a646b52c63",
+    "url": "/index.html"
+  }
+];
