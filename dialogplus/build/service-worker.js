c8f2b66b03a4ce6e2b1ed67ffdde1a6ca72e4855
suyog-pipliwaal <suyogpipliwal@gmail.com>
2024-09-06 02:57:07 +0100

    some more changes

--- dialogplus/build/service-worker.js
+++ dialogplus/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.178941bac098b2bc49858e7f4b028f7c.js"
+  "/precache-manifest.aa15a039ed3658c2a0048defaa6a2d82.js"
 );
 
 workbox.clientsClaim();
